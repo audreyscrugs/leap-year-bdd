62d4c41b3b5a244317739d52d239e530029cdf57
Guest User <student@epicodus.com>
2015-10-19 19:07:51 -0700

    Add html and jQuery functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 var leapYear = function(year) {
 
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
     return true;
   } else {
     return false;
   }
 };
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+    if (!result) {
+      $(".not").text("not");
+    }
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
